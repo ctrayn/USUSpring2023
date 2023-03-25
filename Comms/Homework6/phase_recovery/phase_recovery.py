dd9817f81f351019c10380b8d4919bb700ccdf19
Calvin Passmore <calp2drummer@gmail.com>
2023-03-25 17:47:00 -0600

    Coms HW6

--- Comms/Homework6/phase_recovery/phase_recovery.py
+++ Comms/Homework6/phase_recovery/phase_recovery.py
@@ -1,15 +1,16 @@
 #!/bin/python3
 import differential
 from tx_rx import *
+from pll import PLL
 from pulses import srrc1
 from random import random
 import numpy as np
 import matplotlib.pyplot as plt
-from math import cos, sin
+from math import cos, sin, atan2
 
 K = 1
 N = 100
-Lp = 60
+Lp = 100
 Ts = 1
 alpha = 1
 pulse = srrc1(alpha, N, Lp, Ts)
@@ -18,7 +19,7 @@ pulse = srrc1(alpha, N, Lp, Ts)
 # Transmit
 ########################
 
-num_bits = 100
+num_bits = 1000
             # Every offset    + Random bits
 sent_bits = [1,1,1,0,0,1,0,0] + [int(random() > 0.5) for _ in range(num_bits)]
 
@@ -30,33 +31,34 @@ Q_up = upsample(Q, N)
 I_shaped = np.convolve(I_up, pulse)
 Q_shaped = np.convolve(Q_up, pulse)
 
-plt.figure()
-plt.subplot(2,1,1)
-plt.title("I&Q")
-plt.ylabel("I")
-plt.plot(I_shaped)
-plt.subplot(2,1,2)
-plt.ylabel("Q")
-plt.plot(Q_shaped)
-plt.savefig('Shaped_Pulses.png', format='png')
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.title("Transmitted Signal")
+# plt.ylabel("I")
+# plt.plot(I_shaped)
+# plt.subplot(2,1,2)
+# plt.ylabel("Q")
+# plt.plot(Q_shaped)
 
 
 ########################
 # Receiver
 ########################
 
-I_received = [I + np.random.normal(0,1) for I in I_shaped] #Add signal
-Q_received = [Q + np.random.normal(0,1) for Q in Q_shaped]
+I_received = [I for I in I_shaped] #Add signal
+Q_received = [Q for Q in Q_shaped]
 
-plt.figure()
-plt.subplot(2,1,1)
-plt.title("Noisy Signal")
-plt.ylabel("I")
-plt.plot(I_received)
-plt.subplot(2,1,2)
-plt.ylabel("Q")
-plt.plot(Q_received)
-plt.savefig("NoisyFigure.png", format='png')
+# I_received = [I + np.random.normal(0,1) for I in I_shaped] #Add signal
+# Q_received = [Q + np.random.normal(0,1) for Q in Q_shaped]
+
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.title("Noisy Signal")
+# plt.ylabel("I")
+# plt.plot(I_received)
+# plt.subplot(2,1,2)
+# plt.ylabel("Q")
+# plt.plot(Q_received)
 
 xt = matched_filter(I_received, pulse)
 yt = matched_filter(Q_received, pulse)
@@ -67,28 +69,67 @@ yt_sampled = sample_signal(yt, N, Lp)
 xt_normal = normalize_amplitude(xt_sampled, 1)
 yt_normal = normalize_amplitude(yt_sampled, 1)
 
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.title("Sampled Signal")
+# plt.ylabel("X")
+# plt.stem(xt_normal)
+# plt.subplot(2,1,2)
+# plt.ylabel("Y")
+# plt.stem(yt_normal)
+
 plt.figure()
-plt.subplot(2,1,1)
-plt.title("X&Y")
-plt.ylabel("X")
-plt.stem(xt_normal)
-plt.subplot(2,1,2)
-plt.ylabel("Y")
-plt.stem(yt_normal)
-plt.savefig("sampled_X_Y.png", format='png')
+plt.scatter(xt_normal, yt_normal)
+plt.title("Recieved I, Q")
+plt.xlabel("I")
+plt.ylabel("Q")
 
 ########################
 # Rotation & Decision & PLL
 ########################
 
 theta = 0
+xt_prime = []
+yt_prime = []
+pll = PLL()
+theta_hats = []
+delta_thetas = []
 for index in range(len(xt_normal)):
     a0, a1 = slice_QPSK(xt_normal[index], yt_normal[index])
-    theta = PLL(xt_normal[index], yt_normal[index], theta)
-    x_prime = K * ((a0 * cos(theta)) - (a1 * sin(theta)))
+    # theta = atan2(xt_normal[index], yt_normal[index])
+    # theta_hat = pll.pll(theta)
+    theta_hat = pll.pll(xt_normal[index], yt_normal[index], a0, a1)
+    theta_hats.append(theta_hat)
+    delta_theta = theta_hat
+    delta_thetas.append(delta_theta)
+    xt_prime.append(K * ((a0 * cos(delta_theta)) - (a1 * sin(delta_theta))))
+    yt_prime.append(K * ((a0 * sin(delta_theta)) + (a1 * cos(delta_theta))))
 
-xt_prime = xt_normal
-yt_prime = yt_normal
+plt.figure()
+plt.subplot(2,1,1)
+plt.title("Phase Locked Signal")
+plt.ylabel("X")
+plt.stem(xt_prime)
+plt.subplot(2,1,2)
+plt.ylabel("Y")
+plt.stem(yt_prime)
+
+plt.figure()
+plt.scatter(xt_prime, yt_prime)
+plt.title("IQ PLL out")
+plt.xlabel("I")
+plt.ylabel("Q")
+
+plt.figure()
+plt.plot(theta_hats)
+plt.title("Theta Hat")
+plt.figure()
+plt.plot(delta_thetas)
+plt.title("Delta Theta")
+
+plt.figure()
+plt.plot(pll.error_signal)
+plt.title("Error Signal")
 
 ########################
 # Points to bits
@@ -101,9 +142,20 @@ y_decide = [0 if y < 0 else 1 for y in yt_prime]
 ########################
 r_bits = differential.decoder(x_decide, y_decide)
 
-print(f"Do the bits match? {'Yes' if r_bits == sent_bits else 'No'}")
-print("Sent bits")
-print(sent_bits)
-print("Received bits")
-print(r_bits)
 
+########################
+# Comparisons
+########################
+
+yes = '\033[32mYes\033[0m'
+no = '\033[31mNo\033[0m'
+
+print(f"Do the bits match? {yes if r_bits == sent_bits else no}")
+missed_count = 0
+for index in range(len(sent_bits)):
+    if sent_bits[index] != r_bits[index]:
+        missed_count += 1
+print(f"Error rate {missed_count / len(sent_bits)}")
+
+
+plt.show()
---
+++ Comms/Homework6/phase_recovery/pll.py
@@ -0,0 +1,36 @@
+from math import sqrt, pi, cos, sin, atan2
+
+class PLL:
+    def __init__(self) -> None:
+        self.BnT = 0.5
+        self.Z = 1 / sqrt(2)
+        self.Kp = 7
+        self.K0 = 1
+        self.compute_K1_K2(self.BnT, self.Z, self.K0, self.Kp)
+        self.Omega_n = 1e9
+        self.A = 1
+        self.delayed_k2 = 0
+        self.delayed_dds = 0
+        self.theta_hat = 130
+        self.error_signal = []
+
+    def compute_K1_K2(self, BnT, Z, K0, Kp):
+        K0_Kp_K1 = (4 * Z * BnT / (Z + 1 / (4 * Z)))  / (1 + 2 * Z * BnT / (Z + 1 / (4 * Z)) + (BnT / (Z + 1 / (4 * Z)))**2)
+        K0_Kp_K2 = (4 * (BnT / (Z + 1 / (4 * Z)))**2) / (1 + 2 * Z * BnT / (Z + 1 / (4 * Z)) + (BnT / (Z + 1 / (4 * Z)))**2)
+
+        self.K1 = K0_Kp_K1 / (K0 * Kp)
+        self.K2 = K0_Kp_K2 / (K0 * Kp)
+
+    def pll(self, x, y, a0, a1):
+        theta_error = cos(atan2(x, y)) * -sin(atan2(a0, a1))
+        self.error_signal.append(theta_error)
+        return theta_error
+
+        # Loop Filter
+        self.delayed_k2 = (theta_error * self.K2) + self.delayed_k2
+        v = self.delayed_k2 + (theta_error * self.K1)
+
+        # DDS
+        self.theta_hat = self.delayed_dds
+        self.delayed_dds = -self.delayed_dds + (self.K0 * v)
+        return cos(self.theta_hat) + (pi/4)
