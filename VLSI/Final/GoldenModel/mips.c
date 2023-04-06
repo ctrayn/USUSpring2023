38ce17cdac68e746d3b43bb65c81fb1d3e0c93fb
Calvin Passmore <calp2drummer@gmail.com>
2023-04-06 13:21:54 -0600

    VLSI working

--- VLSI/Final/GoldenModel/mips.c
+++ VLSI/Final/GoldenModel/mips.c
@@ -34,10 +34,10 @@ instruction_t fib_assembly[ASSEMBLY_LEN] = {
     {.opcode = STR,  .dest = 0x4,      .src1 = NULL_REG, .src2 = REG_0},    // Put the XOR in the memory
     {.opcode = POP,  .dest = REG_0,    .src1 = NULL_REG, .src2 = NULL_REG}, // Get the AND value
     {.opcode = STR,  .dest = 0x5,      .src1 = NULL_REG, .src2 = REG_0},    // Store the AND value in memory
-    {.opcode = JMP,  .dest = 24,       .src1 = NULL_REG, .src2 = NULL_REG}, // JUMP Across the next store
+    {.opcode = JMP,  .dest = 27,       .src1 = NULL_REG, .src2 = NULL_REG}, // JUMP Across the next store
+    {.opcode = NOOP, .dest = NULL_REG, .src1 = NULL_REG, .src2 = NULL_REG },
     {.opcode = STR,  .dest = 0x6,      .src1 = NULL_REG, .src2 = REG_3},    // This should be skipped, if it isn't memory 6 will be the fib number
     
-    {.opcode = NOOP, .dest = NULL_REG, .src1 = NULL_REG, .src2 = NULL_REG },
     {.opcode = NOOP, .dest = NULL_REG, .src1 = NULL_REG, .src2 = NULL_REG },
     {.opcode = NOOP, .dest = NULL_REG, .src1 = NULL_REG, .src2 = NULL_REG },
     {.opcode = NOOP, .dest = NULL_REG, .src1 = NULL_REG, .src2 = NULL_REG },
