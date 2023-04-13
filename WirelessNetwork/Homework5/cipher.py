2aeb8cfad8da8d2ce40ba10e98b00476630b0b86
Calvin Passmore <calp2drummer@gmail.com>
2023-04-13 10:54:56 -0600

    Wireless HW5 Cipher workin

--- WirelessNetwork/Homework5/cipher.py
+++ WirelessNetwork/Homework5/cipher.py
@@ -1,40 +1,34 @@
 import math
+import numpy
 from itertools import permutations
 
 cipher = "aauancvlrerurnndltmeaeepbytusticeatnpmeyiicgogorchsrsocnntiiimihaoofpagsivttpsitlbolrotoex"
-possible_lens = [2, 3, 5, 6, 9, 10, 15, 18, 30, 45]
+possible_lens = [15]
 
 for row_len in possible_lens:
     ## Make a table from the cipher
     cipher_table = []
     for row in range(math.ceil(len(cipher)/row_len)):
         to_append = list(cipher[row*row_len:row*row_len+row_len])
-        # while len(to_append) < 8:
-        #     to_append.append(0) # Zero pad
+        while len(to_append) < row_len:
+            to_append.append('0') # Zero pad
         cipher_table.append(to_append)
-    # print(cipher_table)
+    tran_table = numpy.transpose(cipher_table)
+    print(cipher_table)
+    print(tran_table)
 
-    possible_orders = permutations(range(row_len))
+    possible_orders = permutations(range(len(tran_table[0])))
     for index_order in possible_orders:
         # print(index_order)
-
-        ## Reorder the table
-        ordered_table = []
-        for row in cipher_table:
-            # print(row)
-            ordered_table.append([])
+        output_string = ''
+        for row in tran_table:
             for index in index_order:
-                ordered_table[-1].append(row[index])
-        # print(ordered_table)
-
-        output_string = ""
-        for row in ordered_table:
-            for letter in row:
-                output_string += letter
-
+                output_string += row[index]
+        while '0' in output_string:
+            output_string = output_string.replace('0','')
+        print(f'{index_order}{output_string}')
         if "computer" in output_string:
-            print(output_string)
-            print("EUREKA!")
+            print("Found")
             quit(1)
-    print(output_string)
+print("Not found")
 
