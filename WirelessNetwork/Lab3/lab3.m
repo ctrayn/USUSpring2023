f2b0403bbae4cd7c9f56ca32f4ebd685573310b4
Calvin Passmore <calp2drummer@gmail.com>
2023-04-18 09:13:59 -0600

    Wireless Lab3 save

--- WirelessNetwork/Lab3/lab3.m
+++ WirelessNetwork/Lab3/lab3.m
@@ -6,10 +6,10 @@ N0 = -114; %dBm
 alpha = 4;
 reuse = 1/3;
 
-target_SINR_db = linspace(-10, 20, 1000);
+target_SINR_db = linspace(-10, 20, 50);
 results = simuation(radius, N0, P, reuse, alpha, target_SINR_db);
 
 figure();
-plot(target_SINR_db, results);
+plot(target_SINR_db, results, 'o');
 xlabel("Target SINR db")
 ylabel("Converge probability")
--- WirelessNetwork/Lab3/simuation.m
+++ WirelessNetwork/Lab3/simuation.m
@@ -12,14 +12,16 @@ function results = simuation(radius, N0, P, reuse, alpha, target_SNR_dB)
         [0 -radius*sqrt(3)]
     ];
     
-    num_sim = 5000;
+    num_sim = 3000;
     results = zeros(1, length(target_SNR_dB));
     
     for index = 1:length(target_SNR_dB)
-        sum = 0;
+        sum_of_sims = 0;
         for n = 1:num_sim
             % Randomize the position of the user, and calulate the distances to each BS
-            user = [rand(1)*radius rand(1)*radius];
+            r = rand(1)*radius;
+            theta = rand(1)*2*pi;
+            user = [r*cos(theta) r*sin(theta)];
             distances = zeros(1,num_stations);
             for bs = 1:num_stations
                 distances(bs) = sqrt((base_stations(bs,1) - user(1))^2 + (base_stations(bs,2) - user(2))^2);
@@ -28,16 +30,13 @@ function results = simuation(radius, N0, P, reuse, alpha, target_SNR_dB)
             % Get the approximate SINR
             h = exprnd(1);
             numerator = h * distances(1)^(-alpha) * P;
-            denominator = 0;
-            for m = 2:length(distances)
-                g = exprnd(1);
-                denominator = denominator + (g * distances(m)^(-alpha) * P);
-            end
-            SINR = numerator / denominator;
-            SINR_db = (10 * log10(SINR)) + N0;
+            g = exprnd(1);
+            denominator = (g .* distances(2:end).^(-alpha) .* P);
+            SINR = numerator / sum(denominator) + N0;
+            %SINR_db = (10 * log10(SINR)) + N0;
 
-            sum = sum + erf(SINR_db/target_SNR_dB(index));
+            sum_of_sims = sum_of_sims + erf(SINR/target_SNR_dB(index));
         end
-        results(index) = sum/num_sim;
+        results(index) = sum_of_sims/num_sim;
     end
 end
